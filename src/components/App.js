be58b8ef7dd7da01d267428bb1914b5a69c556bc
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-03-18 15:02:17 -0400

    moved components into separate folder and added answer selection functionality

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,15 @@ const initialState = {
   questions: [],
 
   // 'loading', 'error', 'ready', 'active', 'finished'
-  status: 'loading'
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0
 }
 
 function reducer(state, action) {
   switch(action.type) {
+    // if question data is received
     case 'dataReceived':
       return {
         ...state,
@@ -22,25 +26,41 @@ function reducer(state, action) {
         status: 'ready'
       }
 
+    // if question data was not received
     case 'dataFailed':
       return {
         ...state,
         status: 'error'
       }
     
-      case 'start':
-        return {
-          ...state,
-          status: 'active'
-        }
+    // when the user clicks start test button
+    case 'start':
+      return {
+        ...state,
+        status: 'active'
+      }
+    
+    // when the user selects a new answer
+    case 'newAnswer':
+      const question = state.questions.at(state.index)
 
+      return {
+        ...state,
+        answer: action.payload,
+        points: 
+          action.payload === question.correctOption 
+            ? state.points + question.points
+            : state.points
+      }
+
+    // default case throws error
     default:
       throw new Error("Actions unknown")
   }
 }
 
 export default function App() {
-  const [{questions, status}, dispatch] = useReducer(reducer, initialState)
+  const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState)
 
   const numQuestions = questions.length
 
@@ -67,7 +87,13 @@ export default function App() {
           />
         }
 
-        {status === 'active' && <Question />}
+        {status === 'active' && 
+          <Question 
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        }
       </Main>
     </div>
   )
