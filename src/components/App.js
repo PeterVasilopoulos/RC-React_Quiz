5d61f54d539939844a9edcde4f7cf19e4c2fddfb
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-03-19 13:25:47 -0400

    added progress bar and point tracker

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Error from './Error'
 import StartScreen from './StartScreen'
 import Question from './Question'
 import NextButton from './NextButton'
+import Progress from './Progress'
 
 const initialState = {
   questions: [],
@@ -69,9 +70,12 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState)
+  const [{questions, status, index, answer, points}, dispatch] = useReducer(reducer, initialState)
 
   const numQuestions = questions.length
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points, 0
+  )
 
   useEffect(function() {
     fetch('http://localhost:8000/questions')
@@ -98,6 +102,14 @@ export default function App() {
 
         {status === 'active' && 
           <>
+            <Progress 
+              index={index} 
+              numQuestions={numQuestions} 
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+
             <Question 
               question={questions[index]}
               dispatch={dispatch}
